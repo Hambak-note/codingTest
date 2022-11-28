bd6460a3f9e705dea26c3ad0a2bc964db147ee6e
Hambak-note <dudghks5722@gmail.com>
2022-11-28 09:59:34 +0900

    feat: 프로그래머스 level0 옷가게 할인 받기 문제 솔루션

---
+++ programmers/src/level0/GetDiscountClothingStore.java
@@ -0,0 +1,30 @@
+package level0;
+
+public class GetDiscountClothingStore {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int solution(int price) {
+            int answer = 0;
+
+            if(price >= 500000){
+                answer = (int) (price - price * 0.2);
+                return answer;
+            }
+
+            if(price >= 300000){
+                answer = (int) (price - price * 0.1);
+                return answer;
+            }
+
+            if(price >= 100000){
+                answer = (int) (price - price * 0.05);
+            }
+
+            return answer;
+        }
+    }
+}
