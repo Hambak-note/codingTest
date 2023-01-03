7b8708b5d60309f9b4a2be90a474023bc6cee9f1
Hambak-note <dudghks5722@gmail.com>
2023-01-03 21:20:40 +0900

    Create project to practice algorithm with barrrkingdog lecture

---
+++ baaarkingdog/src/main/java/com/example/baaarkingdog/BaaarkingdogApplication.java
@@ -0,0 +1,13 @@
+package com.example.baaarkingdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaaarkingdogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaaarkingdogApplication.class, args);
+    }
+
+}
---
+++ baaarkingdog/src/test/java/com/example/baaarkingdog/BaaarkingdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baaarkingdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaaarkingdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
